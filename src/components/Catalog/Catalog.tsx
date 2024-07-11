7ccbe431acced4f20c8a7166baec2ac88c51eda2
boraneak <50433970+boraneak@users.noreply.github.com>
2024-07-11 11:26:27 +0700

    Refactor: Update useEffect to use DataService for fetching countries

--- src/components/Catalog/Catalog.tsx
+++ src/components/Catalog/Catalog.tsx
@@ -1,40 +1,67 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import { Image } from "antd";
 import { Country } from "../types";
+import { fetchCountries } from "../DataService/DataService";
 
 const Catalog: React.FC = () => {
   const [countries, setCountries] = useState<Country[]>([]);
 
   useEffect(() => {
-    const fetchCountries = async () => {
+    const loadCountries = async () => {
       try {
-        const response = await axios.get("https://restcountries.com/v3.1/all");
-        setCountries(response.data);
+        const countries = await fetchCountries();
+        setCountries(countries);
       } catch (error) {
-        console.error("Error fetching countries:", error);
+        console.error("Error loading countries:", error);
       }
     };
-
-    fetchCountries();
+    loadCountries();
   }, []);
 
   return (
     <>
-      <h1>This is my countries catalog</h1>
       {countries.map((country) => (
         <div key={country.cca3}>
+          <h3>Flag:</h3>
+
           <Image
             width={200}
             src={country.flags.png}
             alt={`${country.name.common} flag`}
           />
+          <h3>Country Name:</h3>
           <h2>{country.name.official}</h2>
           <p>{country.capital}</p>
+          <h3>2 Character Country Code:</h3>
           <p>{country.cca2}</p>
+          <h3>3 Character Country Code:</h3>
+
           <p>{country.cca3}</p>
-          {/* <p>Native name:</p> */}
-          <p>{country.altSpellings[1]}</p>
+          <div>
+            <h3>Native name:</h3>
+            {country.name.nativeName ? (
+              Object.entries(country.name.nativeName).map(([lang, names]) => (
+                <div key={lang}>
+                  <p>{`${lang}: ${names.official} (${names.common})`}</p>
+                </div>
+              ))
+            ) : (
+              <p>No native names available</p>
+            )}
+          </div>
+          <div>
+            <h3>Alternative Spellings:</h3>
+            {country.altSpellings.length > 0 ? (
+              <div>
+                {country.altSpellings.map((spelling, index) => (
+                  <p key={index}>{spelling}</p>
+                ))}
+              </div>
+            ) : (
+              <p>No alternative spellings available</p>
+            )}
+          </div>
+          <h3>Country code:</h3>
           <p>
             {country.idd.root}
             {country.idd.suffixes}
